4b1df5f5ae793bbe7ea40591fdebf03c2a90a5d3
Nico Deblauwe <nico@bluepundit.eu>
2026-04-22 15:44:00 +0200

    FEATURE :sparkles: Voeg contact pagina toe

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,1 @@
+hallo vanuit de contact view
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index'])->name('home');
+Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
