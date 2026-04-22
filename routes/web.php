a8c2ec2d82c23edfae26e9113ea45c0c442eda66
Nico Deblauwe <nico@bluepundit.eu>
2026-04-22 15:45:17 +0200

    REFACTOR :hammer: Aanpassen routes file

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+ * Publieke sectie website
+ */
 Route::get('/', [\App\Http\Controllers\WelcomeController::class, 'index'])->name('home');
 Route::get('/contact', [\App\Http\Controllers\ContactController::class, 'index']);
 
+/*
+ * User die ingelogd is sectie
+ */
+
+
+/*
+ * Admin sectie
+ */
+
+
+
+/*
+ * Dingen hieronder bekijken we later
+ */
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
 });
