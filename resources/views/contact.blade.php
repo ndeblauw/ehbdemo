e719cc03a4bfffb1dfe84cc84c3c88e379964ed8
Nico Deblauwe <nico@bluepundit.eu>
2026-04-22 15:49:40 +0200

    CONTENT :pencil: Voeg basis layout toe

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,15 @@
 
 </head>
 <body>
+<nav>
+    <a href="/">Home</a>
+    <a href="/contact">Contact</a>
+</nav>
 <h1>Contact pagina</h1>
+<p>Dit is de contact pagina</p>
+<footer>
+    <hr>
+    <p>Copyright 2026 - EhB Demo</p>
+</footer>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,15 @@
 
 </head>
 <body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+    </nav>
     <h1>Welcome to EhB Demo!</h1>
+    <p>Dit is de welcome pagina</p>
+    <footer>
+        <hr>
+        <p>Copyright 2026 - EhB Demo</p>
+    </footer>
 </body>
 </html>
