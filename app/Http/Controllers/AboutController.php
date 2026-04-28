d64ead8324af20ff8239f05b16822d4ca34a4ae9
Nico Deblauwe <nico@bluepundit.eu>
2026-04-28 11:37:40 +0200

    FEATURE :sparkles: Demo of blade functionality

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AboutController extends Controller
 {
     public function index()
     {
-        return view('about');
+        $firstname = "Nico";
+        $familyname = "Deblauwe";
+        $person = "$firstname $familyname";
+        $age = 45;
+
+        $hobbies = ['koken', 'fietsen', 'film kijken', 'web development'];
+
+        $family = [
+            ['name' => 'Katrijn', 'gender' => 'female'],
+            ['name' => 'Aster', 'gender' => 'female'],
+            ['name' => 'Merel', 'gender' => 'female'],
+            ['name' => 'Iben', 'gender' => 'male'],
+            ['name' => 'Nico', 'gender' => 'male'],
+        ];
+
+        $familyAsObject = [
+            (object) ['name' => 'Katrijn', 'gender' => 'female'],
+            (object) ['name' => 'Aster', 'gender' => 'female'],
+            (object) ['name' => 'Merel', 'gender' => 'female'],
+            (object) ['name' => 'Iben', 'gender' => 'male'],
+            (object) ['name' => 'Nico', 'gender' => 'male'],
+        ];
+
+        return view('about', [
+            'person' => $person,
+            'age' => $age,
+            'hobbies' => $hobbies,
+            'family' => $family,
+            'familyAsObject' => $familyAsObject,
+        ]);
     }
 }
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,54 @@
-lege about pagina aangemaakt
+<html>
+<head>
+    <title>About Us</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="px-4">
+    <p>De persoon is {{$person}}.</p>
+
+    @if($person == 'Nico Deblauwe') <b>Supermooie naam!</b> @endif
+
+    <p>Dit is tekst <b>in het vet</b>.</p>
+
+    <p>Zijn/haar leeftijd is {{ $age }}</p>
+
+    <div>
+        @dump($person)
+    </div>
+
+    <h1 class="font-bold text-xl mt-4">Hobbies</h1>
+    <p>{{$person}} heeft {{ count($hobbies) }} hobby's.</p>
+    <ul class="list-disc list-inside">
+    @foreach($hobbies as $hobby)
+        <li>{{ $hobby }}</li>
+    @endforeach
+    </ul>
+
+    <h1  class="font-bold text-xl mt-4">Familie</h1>
+    <p>{{$person}} heeft {{ count($family) }} gezinsleden.</p>
+    <ul class="list-disc list-inside">
+        @foreach($family as $member)
+            <li>
+                {{ $member['name'] }}
+                @if($member['gender'] == 'female')
+                    <span class="text-pink-500">(Female)</span>
+                @else
+                    <span class="text-blue-500">(Male)</span>
+                @endif
+            </li>
+        @endforeach
+    </ul>
+
+    <h1 class="font-bold text-xl mt-4">Familie</h1>
+    <p>{{$person}} heeft {{ count($family) }} gezinsleden.</p>
+    <ul>
+        @foreach($familyAsObject as $member)
+            <li>{{ $member->name }} ({{$member->gender}})</li>
+        @endforeach
+    </ul>
+
+
+
+
+</body>
+</html>
