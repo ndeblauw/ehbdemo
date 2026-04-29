fd04c3212e6ba1b92d7c84e3ceec3ad7acbbbfd9
Nico Deblauwe <nico@bluepundit.eu>
2026-04-29 13:32:23 +0200

    FEATURE ✨ Add Task model, migrations & factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Article;
 use App\Models\Category;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,6 +20,7 @@ class DatabaseSeeder extends Seeder
 
         Category::factory(3)->create();
         Article::factory(20)->create();
+        Task::factory(10)->create();
 
 
     }
