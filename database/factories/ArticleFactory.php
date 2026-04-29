4877b8bf0f80d5938f2466357f536a2dcf6562ce
Nico Deblauwe <nico@bluepundit.eu>
2026-04-29 11:06:52 +0200

    FEATURE :sparkles: Add Article Model to application

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'content' => $this->faker->paragraph(),
+            'author' => $this->faker->name(),
+            'category' => $this->faker->randomElement(['Technology', 'Health', 'Business', 'Entertainment']),
+            'published_at' => $this->faker->date(),
+        ];
+    }
+}
